67741fef929576e005cc3aef14af369919e6c712
Niko Fanucchi <nikofanucchi@gmail.com>
2025-02-24 18:00:38 +0100

    aggiunto esercizio JS

---
+++ esercizio.js
@@ -0,0 +1,29 @@
+let nomeStudente = "Niko";
+let etaStudente = 23;
+let corsoAttivo = true;
+
+let primaParte = "Ciao, io sono ";
+let secondaParte = " e ho ";
+let terzaParte = " anni";
+
+let x = 10;
+let y = 3;
+let somma = x + y;
+let sottrazione = x - y;
+let moltiplicazione = x * y;
+let divisione = x / y;
+
+console.log("Somma:", somma);
+console.log("Sottrazione:", sottrazione);
+console.log("Moltiplicazione:", moltiplicazione);
+console.log("Divisione:", divisione);
+
+console.log("Il nome dello studente è:", nomeStudente);
+console.log("L'età dello studente è:", etaStudente);
+console.log("Lo studente è iscritto al corso?", corsoAttivo);
+
+let fraseCompleta = primaParte + nomeStudente + secondaParte + etaStudente + terzaParte;
+console.log(fraseCompleta);
+
+let colorePreferito = prompt("Qual è il tuo colore preferito?");
+console.log("Il colore preferito inserito è:", colorePreferito);
